a3165c37cab610e6a80e1750f012732f238e145f
Rasmus Jürs <rasmus@jurs.me>
2020-07-22 09:47:10 +0200

    add contact template

---
+++ pages/contact.vue
@@ -0,0 +1,197 @@
+<template
+  ><div>
+    <div class="relative bg-gray-800">
+      <div
+        class="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2"
+      >
+        <img
+          class="w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1559225306-3f60aa7b39a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=720&q=60"
+          alt="Support team"
+        />
+      </div>
+      <div
+        class="relative max-w-screen-xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16"
+      >
+        <div class="md:ml-auto md:w-1/2 md:pl-10">
+          <div
+            class="text-base leading-6 font-semibold uppercase tracking-wider text-gray-300"
+          >
+            Award winning support
+          </div>
+          <h2
+            class="mt-2 text-white text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl sm:leading-10"
+          >
+            We’re here to help
+          </h2>
+          <p class="mt-3 text-lg leading-7 text-gray-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas
+            tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim
+            et fermentum, augue. Aliquet amet volutpat quisque ut interdum
+            tincidunt duis.
+          </p>
+          <div class="mt-8">
+            <div class="inline-flex rounded-md shadow">
+              <a
+                href="#"
+                class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-900 bg-white hover:text-gray-600 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+              >
+                Visit the help center
+                <svg
+                  class="-mr-1 ml-3 h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+                  />
+                  <path
+                    d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-white">
+      <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <div
+          class="max-w-lg mx-auto md:max-w-none md:grid md:grid-cols-2 md:gap-8"
+        >
+          <div>
+            <h2
+              class="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9"
+            >
+              Sales Support
+            </h2>
+            <div class="mt-3">
+              <p class="text-lg leading-7 text-gray-500">
+                Nullam risus blandit ac aliquam justo ipsum. Quam mauris
+                volutpat massa dictumst amet. Sapien tortor lacus arcu.
+              </p>
+            </div>
+            <div class="mt-9">
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <svg
+                    class="h-6 w-6 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                    />
+                  </svg>
+                </div>
+                <div class="ml-3 text-base leading-6 text-gray-500">
+                  <p>
+                    +1 (555) 123 4567
+                  </p>
+                  <p class="mt-1">
+                    Mon-Fri 8am to 6pm PST
+                  </p>
+                </div>
+              </div>
+              <div class="mt-6 flex">
+                <div class="flex-shrink-0">
+                  <svg
+                    class="h-6 w-6 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                    />
+                  </svg>
+                </div>
+                <div class="ml-3 text-base leading-6 text-gray-500">
+                  <p>
+                    support@example.com
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="mt-12 sm:mt-16 md:mt-0">
+            <h2
+              class="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9"
+            >
+              Technical Support
+            </h2>
+            <div class="mt-3">
+              <p class="text-lg leading-7 text-gray-500">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni,
+                repellat error corporis doloribus similique, voluptatibus
+                numquam quam, quae officiis facilis.
+              </p>
+            </div>
+            <div class="mt-9">
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <svg
+                    class="h-6 w-6 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                    />
+                  </svg>
+                </div>
+                <div class="ml-3 text-base leading-6 text-gray-500">
+                  <p>
+                    +1 (555) 123 4567
+                  </p>
+                  <p class="mt-1">
+                    Mon-Fri 8am to 6pm PST
+                  </p>
+                </div>
+              </div>
+              <div class="mt-6 flex">
+                <div class="flex-shrink-0">
+                  <svg
+                    class="h-6 w-6 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                    />
+                  </svg>
+                </div>
+                <div class="ml-3 text-base leading-6 text-gray-500">
+                  <p>
+                    support@example.com
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
